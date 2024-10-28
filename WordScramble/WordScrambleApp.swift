237d0e2f634f6fdbf389d4959cf3bb969698517c
Theo Luciano <62947477+theoluciano@users.noreply.github.com>
2024-10-28 15:33:41 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Theo Luciano on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordScrambleTests/WordScrambleTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleTests.swift
+//  WordScrambleTests
+//
+//  Created by Theo Luciano on 10/28/24.
+//
+
+import Testing
+@testable import WordScramble
+
+struct WordScrambleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
